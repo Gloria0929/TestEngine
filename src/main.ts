7c5fd6f8e16445396340ced80d74ffa53b3a8326
TestEngine Dev <dev@testengine.io>
2026-08-27 14:09:32 +0800

    chore: scaffold Vite + Vue3 + TS + Element Plus project

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import './styles/index.scss'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+const pinia = createPinia()
+app.use(pinia)
+app.use(router)
+app.use(ElementPlus)
+for (const [key, comp] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, comp)
+}
+app.mount('#app')
